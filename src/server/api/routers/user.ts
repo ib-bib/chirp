9c7ffc9f9099e3f9dc7cd79470f7146d85b7d413
Ibrahim Adil <81387641+ib-bib@users.noreply.github.com>
2023-10-19 02:27:32 +0000

    commented out stuff to test prod build

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -13,20 +13,21 @@ export const postRouter = createTRPCRouter({
 
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
+    // .mutation(async ({ ctx, input }) => {
+    .mutation(async ({}) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
+      //   return ctx.db.user.create({
+      //     data: {
+      //       name: input.name,
+      //     },
+      //   });
+      // }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
+      // getLatest: publicProcedure.query(({ ctx }) => {
+      //   return ctx.db.user.findFirst({
+      //     orderBy: { createdAt: "desc" },
+      //   });
+    }),
 });
