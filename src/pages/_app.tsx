7f3d577d806898d2792a9b10d309b1d7f6977319
Ibrahim Adil <81387641+ib-bib@users.noreply.github.com>
2023-10-20 01:13:14 +0000

    some broken files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,15 @@ import { api } from "~/utils/api";
 
 import { ClerkProvider } from "@clerk/nextjs";
 
+
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider 
+    signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}
+    signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
+    {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
